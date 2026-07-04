46d25dc8ba717bbbb2bf080ad4e0b41794d43631
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-04 17:09:50 +0545

    url validation using zod

--- backend/src/routes/urls.route.js
+++ backend/src/routes/urls.route.js
@@ -1,10 +1,12 @@
 import express from "express"
 
 import { createShortUrl, getAllUrls, getAUrl, deleteAUrl, updateUrl } from "../controllers/urls.controller.js";
+import { validate } from "../middlewares/validate.js";
+import { createUrlSchema } from "../validators/url.validator.js";
 
 const router = express.Router();
 
-router.post("/",createShortUrl);
+router.post("/",validate(createUrlSchema) ,createShortUrl);
 router.get("/",getAllUrls);
 router.get("/:id",getAUrl);
 router.delete("/:id",deleteAUrl);
