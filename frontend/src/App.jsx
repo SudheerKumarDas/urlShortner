5ea122a1d0f854d3d3664deffe826b97bc68255d
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-06 14:20:18 +0545

    routes defined

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
-
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Dashboard from './pages/Dashboard'
 function App() {
 
   return (
     <>
       <h1>URL Shortner </h1>
+      <Routes>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+      </Routes>
     </>
   )
 }
