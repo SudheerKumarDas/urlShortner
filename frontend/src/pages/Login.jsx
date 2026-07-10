49ff99c05080a3f0b9e1bd2ba628202ba677908a
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-10 21:49:44 +0545

    login from frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,32 +1,58 @@
 import { useState } from "react";
+import api from "../services/api.js";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
-    const handleSubmit = () => {
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post("/auth/login", {
+        email,
+        password,
+      });
+      const responseData = response.data;
+      const user = responseData.user;
+      const username = user.username;
+      const userEmail = user.email;
+      console.log(username);
+      console.log(userEmail);
+      navigate('/dashboard');
+    } catch (error) {
+        console.error(error);
     }
+  };
   return (
     <div>
-        <h2>Login Page</h2>
-        <form onSubmit={handleSubmit}>
+      <h2>Login Page</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email : </label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-            <label htmlFor="email">Email : </label>
-            <input type="text" name="email" placeholder="Enter your email" />
+        <label htmlFor="password">Password : </label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-            <label htmlFor="password">Password : </label>
-            <input type="password" name="password" placeholder="Enter your password"/>
+        <button type="submit">Login</button>
 
-            <button type="submit">Login</button>
-            
-            <div>
-                <p>Don't have account ? </p>
-                <span>Register</span>
-            </div>
-            
-        </form>
+        <div>
+          <p>Don't have account ? </p>
+          <span>Register</span>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
