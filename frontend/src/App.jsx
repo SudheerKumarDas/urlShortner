e77ad67361e7667bf8b8d5e7ea2db3914a14f18d
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-10 23:08:47 +0545

    separate auth service to call backend api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
 import Dashboard from './pages/Dashboard'
+import Landing from './pages/Landing.jsx'
 function App() {
 
   return (
     <>
       <h1>URL Shortner </h1>
       <Routes>
+        <Route path='/' element={<Landing/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/register' element={<Register/>} />
         <Route path='/dashboard' element={<Dashboard/>} />
