ce932caa15a90f2a25003c526a8ada9d65738f2c
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-10 22:54:52 +0545

    error msg and loading state

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,31 +4,32 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 function Login() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-  
-    const [isLoading,setIsLoading] = useState(false);
+  //   const [email, setEmail] = useState("");
+  //   const [password, setPassword] = useState("");
 
-    const [formData,setFormData]=useState({
-        email:"",
-        password:""
-    })
+  const [isLoading, setIsLoading] = useState(false);
+  const [error,setError] = useState("");
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
-        ...formData,
-        [e.target.name]:e.target.value
-    })
-  }
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
     try {
       const response = await api.post("/auth/login", {
-        email:formData.email,
-        password:formData.password,
+        email: formData.email,
+        password: formData.password,
       });
       const responseData = response.data;
       const user = responseData.user;
@@ -36,16 +37,19 @@ function Login() {
       const userEmail = user.email;
       console.log(username);
       console.log(userEmail);
-      navigate('/dashboard');
-    } catch (error) {
-        console.error(error);
-    } finally{
-        setIsLoading(false);
+      navigate("/dashboard");
+    } catch (err) {
+        setError(
+            err.response?.data?.message || "Something went wrong"
+        )
+    } finally {
+      setIsLoading(false);
     }
   };
   return (
     <div>
       <h2>Login Page</h2>
+      {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email : </label>
         <input
@@ -65,13 +69,13 @@ function Login() {
           onChange={handleChange}
         />
 
-        <button type="submit" disabled={isLoading}>Login</button>
+        <button type="submit" disabled={isLoading}>
+          {isLoading?"Logging...":"Login"}
+        </button>
 
         <div>
           <p>Don't have account ? </p>
-          <Link to="/register">
-                Register
-          </Link>
+          <Link to="/register">Register</Link>
         </div>
       </form>
     </div>
