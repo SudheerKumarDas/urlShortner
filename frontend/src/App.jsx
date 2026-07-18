8ba4b563e38b3b694fb4345688bc72b6f8902853
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-18 15:03:59 +0545

    navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
 
   return (
     <div className='w-full h-screen bg-zinc-500'>
-      <h1>URL Shortner </h1>
+      <Navbar/>
     </div>
   )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+  return (
+    <header className="flex justify-around items-center pt-3">
+        <h2 className="text-2xl">URL Shortner</h2>
+        <div className="flex items-center gap-6">
+            <h3>Sudheer</h3>
+            <button>Logout</button>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
