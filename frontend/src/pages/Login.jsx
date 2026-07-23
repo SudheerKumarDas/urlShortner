f0b66f0d1c858d88064630a0b60b019f05b6fcbd
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-24 00:25:44 +0545

    done for the day

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
 
 const Login = () => {
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+
   return (
-    <div>Login</div>
+    <div className="w-full h-screen">
+        <form className="max-w-2xl  flex justify-center items-center bg-amber-300">
+
+        <div className="flex flex-col">
+            <label htmlFor="email">Email : </label>
+            <input 
+                type="text"
+                name="email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)} 
+            />
+        </div>
+
+        <div className="flex flex-col">
+            <label htmlFor="password">Password : </label>
+            <input 
+                type="text"
+                name="password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)} 
+            />
+        </div>
+    </form>
+    </div>
   )
 }
 
