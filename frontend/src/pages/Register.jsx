5fe7802a5f12f214e43d3f7ea71742bee86934e7
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-25 09:32:20 +0545

    register frontend

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,44 @@
+import { useState } from "react";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Register
+  return (
+    <form className="max-w-2xl h-screen flex justify-center items-center">
+
+      <div className="flex flex-col">
+        <label htmlFor="username">Username : </label>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <label htmlFor="email">Email : </label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <label htmlFor="password">Password : </label>
+        <input
+          type="text"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Register;
