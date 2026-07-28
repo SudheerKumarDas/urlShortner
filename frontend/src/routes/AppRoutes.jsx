0dec9dabd8ae439c3e4ec4967df1dc82e22e9083
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-07-28 11:31:06 +0545

    error fixed

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,13 +4,23 @@ import Login from "../pages/Login.jsx";
 import Dashboard from "../pages/Dashboard.jsx";
 import ForgetPassword from "../pages/ForgetPassword.jsx";
 import ProtectedRoute from "../components/ProtectedRoute.jsx";
+import { Navigate } from "react-router-dom";
 
-const AppRoutes = ({isAuthenticated,setIsAuthenticated}) => {
+const AppRoutes = ({ isAuthenticated, setIsAuthenticated }) => {
   return (
     <div>
       <Routes>
+        <Route
+          path="/"
+          element={
+            <Navigate to={isAuthenticated ? "/dashboard" : "/login"} replace />
+          }
+        />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+        <Route
+          path="/login"
+          element={<Login setIsAuthenticated={setIsAuthenticated} />}
+        />
         <Route
           path="/dashboard"
           element={
