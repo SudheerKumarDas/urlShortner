ca32fe19368123b818f87ef7ed084dbf9d02c63d
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-08-03 11:03:55 +0545

    email service

---
+++ backend/src/services/email.service.js
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+import { email } from "zod";
+
+const transporter = nodemailer.createTransport({
+  host: "stmp.gmail.com",
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const emailVerification = async (toEmail, token) => {
+  const verifyUrl = `process.env.CLIENT_URL/api/auth/verify-email?token=${token}`;
+  await transporter.sendMail({
+    from: `"MY APP" <${process.env.EMAIL_USER}>`,
+    to: toEmail,
+    subject: "Verify email",
+    html: `
+            <h2>Email Verification</h2>
+      <p>Click the link below to verify your email. This link expires in 1 hour.</p>
+      <a href="${verifyUrl}" target="_blank">Verify Email</a>
+      <p>If the button doesn't work, copy this link: ${verifyUrl}</p>
+
+        `,
+  });
+};
+
+export default emailVerification;
